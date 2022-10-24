d798686b0c63e751dfe77a8c1eea99acf83a1efc
Grabsky <grabskypl@gmail.com>
2022-10-24 22:19:51 +0200

    improve readability of exception messages

--- core/src/main/java/grabsky/configuration/exception/ConfigurationLoadException.java
+++ core/src/main/java/grabsky/configuration/exception/ConfigurationLoadException.java
@@ -2,8 +2,8 @@ package grabsky.configuration.exception;
 
 import java.io.File;
 
-import static grabsky.configuration.util.ConsoleColor.RED;
 import static grabsky.configuration.util.ConsoleColor.RESET;
+import static grabsky.configuration.util.ConsoleColor.YELLOW;
 
 /**
  * Use {@link #getCause} to get instance of exception that caused this exception to be thrown.
@@ -11,7 +11,7 @@ import static grabsky.configuration.util.ConsoleColor.RESET;
 public final class ConfigurationLoadException extends RuntimeException {
 
     public ConfigurationLoadException(final Class<?> clazz, final File file, final Throwable cause) {
-        super("Configuration class '" + RED + clazz.getName() + RESET + "' failed to load contents of '" + RED + file.getPath() + RESET + "'", cause);
+        super("Configuration class " + YELLOW + clazz.getName() + RESET + " failed to load contents of " + YELLOW + file.getPath() + RESET, cause);
     }
 
 }
