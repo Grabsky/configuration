3982e1fff3cc6091bdf69793bd29cb2cd90ffb00
Grabsky <grabskypl@gmail.com>
2022-10-28 22:10:14 +0200

    remove colors from exception messages

--- core/src/main/java/grabsky/configuration/exception/ConfigurationException.java
+++ core/src/main/java/grabsky/configuration/exception/ConfigurationException.java
@@ -2,13 +2,10 @@ package grabsky.configuration.exception;
 
 import java.io.File;
 
-import static grabsky.configuration.util.ConsoleColor.RESET;
-import static grabsky.configuration.util.ConsoleColor.YELLOW;
-
 /** Use {@link #getCause} to get instance of exception that caused this exception to be thrown. */
-public class ConfigurationException extends Exception {
+public class ConfigurationException extends RuntimeException {
 
-    public ConfigurationException(final Class<?> clazz, final File file, final Throwable cause) {
-        super("Configuration class " + YELLOW + clazz.getName() + RESET + " failed to load contents of " + YELLOW + file.getPath() + RESET, cause);
+    public ConfigurationException(final Class<?> configurationClass, final File file, final Throwable cause) {
+        super("[" + configurationClass.getName() + "]  An error occurred during mapping of: " + file.getPath(), cause);
     }
 }
--- paper/src/main/java/grabsky/configuration/paper/exception/JsonSerializationException.java
+++ paper/src/main/java/grabsky/configuration/paper/exception/JsonSerializationException.java
@@ -1,6 +1,5 @@
 package grabsky.configuration.paper.exception;
 
-import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -9,15 +8,8 @@ import java.lang.reflect.Type;
 
 public class JsonSerializationException extends JsonParseException {
 
-    private static final String RESET = "\u001B[0m";
-    private static final String YELLOW = "\u001B[33m";
-
-    public JsonSerializationException(@NotNull final Type expected, @Nullable final JsonElement value) {
-        super("Could not create a valid " + YELLOW + expected.getTypeName() + RESET + " from " + YELLOW + value + RESET);
-    }
-
-    public JsonSerializationException(@NotNull final Type expected, @Nullable final String value) {
-        super("Could not create a valid " + YELLOW + expected.getTypeName() + RESET + " from " + YELLOW + value + RESET);
+    public JsonSerializationException(@NotNull final Type type, @Nullable final Object value) {
+        super("[" + type.getTypeName() + "] An error occurred during (de)serialization of: " + value);
     }
 
 }
